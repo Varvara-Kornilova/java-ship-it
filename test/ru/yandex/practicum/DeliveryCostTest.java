d4e4455d22a006a07835ee97d808e67b451bca5c
Varvara Kornilova <vrvrmoren@yandex.ru>
2025-10-16 01:24:17 +0300

    1й коммит

--- test/ru/yandex/practicum/DeliveryCostTest.java
+++ test/ru/yandex/practicum/DeliveryCostTest.java
@@ -1,4 +1,36 @@
 package ru.yandex.practicum;
 
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.delivery.FragileParcel;
+import ru.yandex.practicum.delivery.PerishableParcel;
+import ru.yandex.practicum.delivery.StandardParcel;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 public class DeliveryCostTest {
+
+    StandardParcel standardParcel = new StandardParcel("Плед", 2, "г. Москва", 1);
+    PerishableParcel perishableParcel = new PerishableParcel("Колбаса", 5, "г. Дмитров",
+            3, 5);
+    FragileParcel fragileParcel = new FragileParcel("Аквариум", 1, "г. Солнечногорск",
+            28);
+
+    @Test
+    public void shouldCalculateDeliveryCostForStandardParcel() {
+        int resultCost = standardParcel.calculateDeliveryCost();
+        assertEquals(4, resultCost);
+    }
+
+    @Test
+    public void shouldCalculateDeliveryCostForPerishableParcel() {
+        int resultCost = perishableParcel.calculateDeliveryCost();
+        assertEquals(15, resultCost);
+    }
+
+    @Test
+    public void shouldCalculateDeliveryCostForFragileParcel() {
+        int resultCost = fragileParcel.calculateDeliveryCost();
+        assertEquals(4, resultCost);
+    }
 }
