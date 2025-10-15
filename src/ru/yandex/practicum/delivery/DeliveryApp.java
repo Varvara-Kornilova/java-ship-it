02b6493dd306adf723182c32dee603446dac1827
Varvara Kornilova <vrvrmoren@yandex.ru>
2025-10-15 20:03:40 +0300

    добавлен пункт меню "показать содержимое коробки"

--- src/ru/yandex/practicum/delivery/DeliveryApp.java
+++ src/ru/yandex/practicum/delivery/DeliveryApp.java
@@ -15,7 +15,7 @@ public class DeliveryApp {
             String input = scanner.nextLine();
 
             if (input.isEmpty()) {
-                System.out.println("Пожалуйста, введите команду в виде числа от 0 до 3.");
+                System.out.println("Пожалуйста, введите команду в виде числа от 0 до 5. \n");
                 continue;
             }
 
@@ -35,25 +35,30 @@ public class DeliveryApp {
                     case 4:
                         parcelController.reportStatus();
                         break;
+                    case 5:
+                        parcelController.showBoxContents();
+                        break;
                     case 0:
                         running = false;
                         break;
                     default:
-                        System.out.println("Неверный выбор.");
+                        System.out.println("Неверный выбор. \n");
                 }
             } catch (NumberFormatException e) {
-                System.out.println("Неверный формат. Пожалуйста, введите номер команды (0, 1, 2 или 3).\n");
+                System.out.println("Неверный формат. Пожалуйста, введите номер команды (от 0 до 5). \n");
             }
         }
     }
 
     private static void showMenu() {
+        System.out.println("");
         System.out.println("Выберите действие:");
         System.out.println("1 — Добавить посылку");
         System.out.println("2 — Отправить все посылки");
         System.out.println("3 — Посчитать стоимость доставки");
         System.out.println("4 - Отследить все посылки");
-        System.out.println("0 — Завершить");
+        System.out.println("5 - Показать содержимое коробки");
+        System.out.println("0 — Завершить \n");
     }
 }
 
